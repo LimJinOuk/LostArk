b6892feb84889e6ea83b7a5c4bf7f27e4c64845e
이경진 <leemmm0226@naver.com>
2026-01-09 09:34:29 +0900

    armoriesAPI 파라미터 추가

--- src/main/java/com/jinouk/lostark/service/armoriesAPIService.java
+++ src/main/java/com/jinouk/lostark/service/armoriesAPIService.java
@@ -12,9 +12,7 @@ public class armoriesAPIService {
     private final WebClient loawebclient;
 
     //치명 , 특화 , 신속 , 제압 , 인내 , 숙련등의 수치및 증감량 조회
-    public Mono<String> getArmoriesCharacterProfile() {
-        //이름은 나중에 입력값으로 바꿀 것.
-        String name = "치킨버거사주세요";
+    public Mono<String> getArmoriesCharacterProfile(String name) {
         return loawebclient
                 .get()
                 .uri("/armories/characters/{name}/profiles", name)
@@ -23,9 +21,7 @@ public class armoriesAPIService {
     }
 
     //사용자 캐릭터가 착용한 장비 조회
-    public Mono<String> getArmoriesCharacterEquipment() {
-        //이름은 나중에 입력값으로 바꿀 것.
-        String name = "치킨버거사주세요";
+    public Mono<String> getArmoriesCharacterEquipment(String name) {
         return loawebclient
                 .get()
                 .uri("/armories/characters/{name}/equipment", name)
@@ -34,9 +30,7 @@ public class armoriesAPIService {
     }
 
     //사용자 캐릭터가 착용한 아바타 장비 조회
-    public Mono<String> getArmoriesCharacterAvatars() {
-        //이름은 나중에 입력값으로 바꿀 것.
-        String name = "치킨버거사주세요";
+    public Mono<String> getArmoriesCharacterAvatars(String name) {
         return loawebclient
                 .get()
                 .uri("/armories/characters/{name}/avatars", name)
@@ -45,9 +39,7 @@ public class armoriesAPIService {
     }
 
     //캐릭터의 스킬과 스킬에 장착한 룬 정보 조회
-    public Mono<String> getArmoriesCharacterCombatSkills() {
-        //이름은 나중에 입력값으로 바꿀 것.
-        String name = "치킨버거사주세요";
+    public Mono<String> getArmoriesCharacterCombatSkills(String name) {
         return loawebclient
                 .get()
                 .uri("/armories/characters/{name}/combat-skills", name)
@@ -56,9 +48,7 @@ public class armoriesAPIService {
     }
 
     //캐릭터의 각인 정보 조회
-    public Mono<String> getArmoriesCharacterEngravings() {
-        //이름은 나중에 입력값으로 바꿀 것.
-        String name = "치킨버거사주세요";
+    public Mono<String> getArmoriesCharacterEngravings(String name) {
         return loawebclient
                 .get()
                 .uri("/armories/characters/{name}/engravings", name)
@@ -67,9 +57,7 @@ public class armoriesAPIService {
     }
 
     //캐릭터의 카드 정보 조회
-    public Mono<String> getArmoriesCharacterCards() {
-        //이름은 나중에 입력값으로 바꿀 것.
-        String name = "치킨버거사주세요";
+    public Mono<String> getArmoriesCharacterCards(String name) {
         return loawebclient
                 .get()
                 .uri("/armories/characters/{name}/cards", name)
@@ -78,9 +66,7 @@ public class armoriesAPIService {
     }
 
     //캐릭터의 보석 정보 조회
-    public Mono<String> getArmoriesCharacterGems() {
-        //이름은 나중에 입력값으로 바꿀 것.
-        String name = "치킨버거사주세요";
+    public Mono<String> getArmoriesCharacterGems(String name) {
         return loawebclient
                 .get()
                 .uri("/armories/characters/{name}/gems", name)
@@ -89,9 +75,7 @@ public class armoriesAPIService {
     }
 
     //캐릭터의 콜로세움(PVP) 정보 조회
-    public Mono<String> getArmoriesCharacterColosseums() {
-        //이름은 나중에 입력값으로 바꿀 것.
-        String name = "치킨버거사주세요";
+    public Mono<String> getArmoriesCharacterColosseums(String name) {
         return loawebclient
                 .get()
                 .uri("/armories/characters/{name}/colosseums", name)
@@ -100,9 +84,7 @@ public class armoriesAPIService {
     }
 
     //캐릭터의 내실 정보 조회
-    public Mono<String> getArmoriesCharacterCollectibles() {
-        //이름은 나중에 입력값으로 바꿀 것.
-        String name = "치킨버거사주세요";
+    public Mono<String> getArmoriesCharacterCollectibles(String name) {
         return loawebclient
                 .get()
                 .uri("/armories/characters/{name}/collectibles", name)
@@ -111,9 +93,7 @@ public class armoriesAPIService {
     }
 
     //캐릭터의 아크 패시브 정보 조회
-    public Mono<String> getArmoriesCharacterArkpassive() {
-        //이름은 나중에 입력값으로 바꿀 것.
-        String name = "치킨버거사주세요";
+    public Mono<String> getArmoriesCharacterArkpassive(String name) {
         return loawebclient
                 .get()
                 .uri("/armories/characters/{name}/arkpassive", name)
@@ -122,9 +102,7 @@ public class armoriesAPIService {
     }
 
     //캐릭터의 아크 그리드 코어 정보 조회
-    public Mono<String> getArmoriesCharacterArkgrid() {
-        //이름은 나중에 입력값으로 바꿀 것.
-        String name = "치킨버거사주세요";
+    public Mono<String> getArmoriesCharacterArkgrid(String name) {
         return loawebclient
                 .get()
                 .uri("/armories/characters/{name}/arkgrid", name)
