b0e23b5fc1a6c7d82720f269034be0629c73a48a
Your Name <you@example.com>
2026-01-30 13:29:37 +0900

    marketApi 5페이지 통합버전

--- src/main/java/com/jinouk/lostark/service/marketsAPIService.java
+++ src/main/java/com/jinouk/lostark/service/marketsAPIService.java
@@ -4,6 +4,7 @@ package com.jinouk.lostark.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
@@ -15,23 +16,27 @@ public class marketsAPIService {
 
     private final WebClient loawebclient;
 
-    // marketAPIService.java
     public Mono<String> getMarketData(String itemName, int categoryCode, String grade) {
-        Map<String, Object> requestBody = new HashMap<>();
-        requestBody.put("Sort", "GRADE");
-        requestBody.put("CategoryCode", categoryCode);
-        requestBody.put("ItemTier", 0);
-        requestBody.put("ItemGrade", grade);
-        requestBody.put("ItemName", itemName);
-        requestBody.put("PageNo", 1);
-        requestBody.put("SortCondition", "ASC");
+        return Flux.range(1, 5)
+                .concatMap(pageNo -> {
+                    Map<String, Object> requestBody = new HashMap<>();
+                    requestBody.put("Sort", "CURRENT_MIN_PRICE");
+                    requestBody.put("CategoryCode", categoryCode);
+                    requestBody.put("ItemTier", 0);
+                    requestBody.put("ItemGrade", grade);
+                    requestBody.put("ItemName", itemName);
+                    requestBody.put("PageNo", pageNo);
+                    requestBody.put("SortCondition", "DESC");
 
-        return loawebclient
-                .post()
-                .uri("/markets/items")
-                .header("Content-Type", "application/json")
-                .bodyValue(requestBody)
-                .retrieve()
-                .bodyToMono(String.class);
+                    return loawebclient
+                            .post()
+                            .uri("/markets/items")
+                            .header("Content-Type", "application/json")
+                            .bodyValue(requestBody)
+                            .retrieve()
+                            .bodyToMono(String.class);
+                })
+                .collectList()
+                .map(results -> "[" + String.join(",", results) + "]");
     }
 }
