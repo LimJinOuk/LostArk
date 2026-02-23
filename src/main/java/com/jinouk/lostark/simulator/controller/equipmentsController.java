c67c2b10bc17ba81360820ddd8487fe015ba6211
Your Name <you@example.com>
2026-02-23 14:12:45 +0900

    아크그리드, 악세사리 - Dto, Controller

--- src/main/java/com/jinouk/lostark/simulator/controller/equipmentsController.java
+++ src/main/java/com/jinouk/lostark/simulator/controller/equipmentsController.java
@@ -1,5 +1,6 @@
 package com.jinouk.lostark.simulator.controller;
 
+import com.jinouk.lostark.simulator.dto.ariGridDto;
 import com.jinouk.lostark.simulator.dto.equipmentsDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -20,11 +21,21 @@ import java.util.Map;
 public class equipmentsController {
 
     @PostMapping("/simulatorEquipments")
-    public String equipments (@RequestBody Map<String, equipmentsDto> equipmentMap) {
-        equipmentMap.forEach((key, value) -> {
+    public String equipments (@RequestBody equipmentsDto weaponDto) {
+        // 성공적으로 매핑되면 DTO에 값이 들어옵니다.
+        System.out.println("수신된 무기 정보: " + weaponDto.toString());
+        return "무기 데이터 수신 성공!";
+    }
+    @PostMapping("/simulatorAccessories")
+    public String accessories (@RequestBody Map<String, equipmentsDto> accessoriesMap) {
+        accessoriesMap.forEach((key, value) -> {
             System.out.println(key + " -> " + value);
         });
-
         return "데이터 수신 성공!";
+    }
+    @PostMapping("/simulatorArkGrid")
+    public String arkGrids (@RequestBody ariGridDto arkGridDto ) {
+        System.out.println("수신된 아크 그리드 정보: " + arkGridDto.toString());
+        return "무기 데이터 수신 성공!";
     }
 }
---
+++ src/main/java/com/jinouk/lostark/simulator/dto/accessoriesDto.java
@@ -0,0 +1,13 @@
+package com.jinouk.lostark.simulator.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class accessoriesDto {
+
+    private String accessoriesName;
+}
