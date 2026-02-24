32c8c99af32aca1bbd36a1e8c79ccf5e6a45e846
이경진 <leemmm0226@naver.com>
2026-02-24 13:21:55 +0900

    시뮬레이터 젬 효과 엔드포인트, Dto 생성

---
+++ src/main/java/com/jinouk/lostark/simulator/dto/gemEffectDto.java
@@ -0,0 +1,14 @@
+package com.jinouk.lostark.simulator.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class gemEffectDto {
+    private double atk_pct;
+    private double add_pct;
+    private double boss_pct;
+}
