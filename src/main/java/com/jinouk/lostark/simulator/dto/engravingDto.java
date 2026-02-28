f6559d89ddaf993fb35d8b704a5bf30171182f6b
somgam6373 <105191945+somgam6373@users.noreply.github.com>
2026-02-28 15:48:47 +0900

    풀 리퀘스트 병합 #13
    * 각인 dto / controller

---
+++ src/main/java/com/jinouk/lostark/simulator/dto/engravingDto.java
@@ -0,0 +1,25 @@
+package com.jinouk.lostark.simulator.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class engravingDto {
+    private String characterName;
+    private List<engravingsList> engravings;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class engravingsList {
+        private String name;
+        private String grade;
+        private Integer level;
+        private Integer abilityStoneLevel;
+    }
+}
