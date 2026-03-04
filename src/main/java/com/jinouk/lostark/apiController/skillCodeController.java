0e7d2842dcc7918029365e2819b3fca1f9e7dab9
LeeGyeongJin <78292636+LeeGJin@users.noreply.github.com>
2026-03-04 20:48:39 +0900

    풀 리퀘스트 병합 #17
    * 시뮬레이터 스킬 구현 중 1

--- src/main/java/com/jinouk/lostark/apiController/skillCodeController.java
+++ src/main/java/com/jinouk/lostark/apiController/skillCodeController.java
@@ -3,12 +3,14 @@ package com.jinouk.lostark.apiController;
 import com.jinouk.lostark.dto.LostarkProfileIds;
 import com.jinouk.lostark.service.LostarkProfileScrapeService;
 import com.jinouk.lostark.service.LostarkSkillRecommendService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class skillCodeController {
 
     private final LostarkProfileScrapeService profileService;
