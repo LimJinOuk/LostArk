83513b51cd0badfa8024f8305d735a905241caf0
LimJinOuk <jinouk038@gmail.com>
2026-03-10 03:38:41 +0900

    버서커 커밋

---
+++ src/main/java/com/jinouk/lostark/simulator/dto/arkPassiveEnlight/berserker.java
@@ -0,0 +1,18 @@
+package com.jinouk.lostark.simulator.dto.arkPassiveEnlight;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class berserker {
+    private double 적주피;
+    private double 공격력;
+    private double 치명타_피해량;
+    private double 치명타_적중률;
+}
