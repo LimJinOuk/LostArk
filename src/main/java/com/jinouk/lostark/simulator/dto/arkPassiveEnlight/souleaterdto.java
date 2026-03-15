786b8d4955285ebd9274c33ae0f690abde3692fe
LimJinOuk <jinouk038@gmail.com>
2026-03-16 02:33:18 +0900

    소울이터 커밋

---
+++ src/main/java/com/jinouk/lostark/simulator/dto/arkPassiveEnlight/souleaterdto.java
@@ -0,0 +1,21 @@
+package com.jinouk.lostark.simulator.dto.arkPassiveEnlight;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class souleaterdto {
+    private double 사신화_상태에서_사신_스킬_피해량;
+    private double 사신화_상태에서_살귀_스킬_피해량;
+    private double 사신화_상태에서_망자_스킬_피해량;
+    private double 강화된_사신_스킬_피해량;
+    private double 사신_스킬_피해량;
+    private double 살귀_스킬_피해량;
+    private double 망자_스킬_피해량;
+    private double 치적;
+}
---
+++ src/main/java/com/jinouk/lostark/simulator/service/ArkPassiveService/job/souleater.java
@@ -0,0 +1,82 @@
+package com.jinouk.lostark.simulator.service.ArkPassiveService.job;
+
+import com.jinouk.lostark.simulator.dto.ArkPassiveDTO;
+import com.jinouk.lostark.simulator.dto.arkPassiveEnlight.souleaterdto;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class souleater {
+
+    public souleaterdto souleater(String title , ArkPassiveDTO dto){
+        souleaterdto souleaterdto = new souleaterdto();
+        Map<String , Map<String , Integer>> nodes = dto.getNodes();
+        Map<String , Integer> enlightment = nodes.get("깨달음");
+
+        double 사신화_상태에서_사신_스킬_피해량 = 0.0;
+        double 사신화_상태에서_살귀_스킬_피해량 = 0.0;
+        double 사신화_상태에서_망자_스킬_피해량 = 0.0;
+        double 강화된_사신_스킬_피해량 = 0.0;
+        double 사신_스킬_피해량 = 0.0;
+        double 살귀_스킬_피해량 = 0.0;
+        double 망자_스킬_피해량 = 0.0;
+        double 치적 = 0.0;
+
+        int 그믐의_경계 = enlightment.get("그믐의 경계");
+        int 죽음_연마 = enlightment.get("죽음 연마");
+        int 허물어진_경계 = enlightment.get("허물어진 경계");
+        int 영혼_강화 = enlightment.get("영혼 강화");
+        int 영혼_제어 = enlightment.get("영혼 제어");
+
+        if (그믐의_경계 > 0){
+            살귀_스킬_피해량 += 40.0 * 그믐의_경계;
+        }
+        if (죽음_연마 > 0) {
+            살귀_스킬_피해량 += 24 * 죽음_연마;
+            사신_스킬_피해량 += 24 * 죽음_연마;
+        }
+        if (허물어진_경계 > 0){
+            강화된_사신_스킬_피해량 += 20.0 * 허물어진_경계;
+        }
+        if (영혼_강화 > 0) {
+            사신_스킬_피해량 += 0.6 * 영혼_강화;
+            살귀_스킬_피해량 += 0.6 * 영혼_강화;
+        }
+        if (영혼_제어 > 0) {
+            살귀_스킬_피해량 += 1.0 * 영혼_제어;
+            망자_스킬_피해량 += 1.0 * 영혼_제어;
+        }
+
+        int 영혼_친화력 = enlightment.get("영혼 친화력");
+        int 만월의_집행자 = enlightment.get("만월의 집행자");
+        int 영혼_증폭 = enlightment.get("영혼 증폭");
+        int 영혼_길잡이 = enlightment.get("영혼 길잡이");
+
+        if (영혼_친화력 > 0){
+            치적 += 3.0 * 영혼_친화력;
+        }
+        if (만월의_집행자 > 0){
+            사신화_상태에서_사신_스킬_피해량 += 3.0 * 만월의_집행자;
+        }
+        if (영혼_증폭 > 0) {
+            사신화_상태에서_사신_스킬_피해량 += 1.6 * 영혼_증폭;
+            사신화_상태에서_망자_스킬_피해량 += 1.6 * 영혼_증폭;
+            사신화_상태에서_살귀_스킬_피해량 += 1.6 * 영혼_증폭;
+        }
+        if (영혼_길잡이 > 0) {
+            사신_스킬_피해량 += 1.2 * 영혼_길잡이;
+        }
+
+        souleaterdto.set사신화_상태에서_사신_스킬_피해량(사신화_상태에서_사신_스킬_피해량);
+        souleaterdto.set사신화_상태에서_살귀_스킬_피해량(사신화_상태에서_살귀_스킬_피해량);
+        souleaterdto.set사신화_상태에서_망자_스킬_피해량(사신화_상태에서_망자_스킬_피해량);
+        souleaterdto.set강화된_사신_스킬_피해량(강화된_사신_스킬_피해량);
+        souleaterdto.set사신_스킬_피해량(사신_스킬_피해량);
+        souleaterdto.set살귀_스킬_피해량(살귀_스킬_피해량);
+        souleaterdto.set망자_스킬_피해량(망자_스킬_피해량);
+        souleaterdto.set치적(치적);
+
+        return souleaterdto;
+    }
+}
